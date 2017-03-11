13834e3df5a4b378a2cd5266a43526a8791d5743
Praveen Gupta <praveen@Praveens-MBP.fios-router.home>
2017-03-11 14:15:51 -0500

    First commit

---
+++ cmake-build-debug/CMakeFiles/receiver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/receiver.dir/gbn.c.o"
+  "CMakeFiles/receiver.dir/receiver.c.o"
+  "receiver.pdb"
+  "receiver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/receiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
